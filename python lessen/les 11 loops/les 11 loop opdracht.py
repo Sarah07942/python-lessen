dad1d8bf4b9ffd1e8a64b5b575e19868684b14d2
Sarah07942 <sarahdiallo2020@gmail.com>
2025-03-31 13:58:47 +0200

    Update les 11 loop opdracht.py

--- python lessen/les 11 loops/les 11 loop opdracht.py
+++ python lessen/les 11 loops/les 11 loop opdracht.py
@@ -4,8 +4,19 @@ import random
 
 aantal_worpen = 0
 totaal_van_de_worpen = 0
-while aantal_worpen != 0:
+aantal_herhaling = int(input("Hoeveel keer worp je de dobbelstenen?:"))
+#De dobbelstenen worden 10 keer weggegooid
+while aantal_worpen < aantal_herhaling :
     eerste_steen = random.randint(1, 6)
     tweede_steen = random.randint(1, 6)
     dobbelsteen_som = eerste_steen + tweede_steen
     print("Je rolde een " + str(dobbelsteen_som) + "!")
+    aantal_worpen = aantal_worpen + 1
+#het totaal van de worpen wordt berekend
+    totaal_van_de_worpen += dobbelsteen_som 
+#Het gemiddelde worp berekenen
+if aantal_herhaling > 0:
+    gemiddelde_worp = totaal_van_de_worpen / aantal_herhaling
+    print("Het gemiddelde worp is" + str(gemiddelde_worp) + ".")
+else:
+    print("Je hebt geen worpen uitgevoerd.")
